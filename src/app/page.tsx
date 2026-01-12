e2fe4f6a826b4663949a6dfbbc2e3479ad1ea6e6
Peu Silva <peusilva@proton.me>
2026-01-12 08:21:52 -0300

    feat: create homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,79 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
+import { AuthForm } from "@/components/forms/auth-form";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+export default function Auth() {
+	return (
+		<main className='min-h-screen w-full bg-background'>
+			<div className='mx-auto w-full max-w-480 min-h-screen flex flex-col'>
+				<div className='flex items-center px-4 sm:px-6 md:px-8 lg:px-8 py-8 sm:py-12 lg:py-16'>
+					<Image
+						src='/nortus-logo.svg'
+						width={174}
+						height={54}
+						className='justify-start'
+						alt='Logo displaying the name Nortus in blue'
+					/>
+				</div>
+
+				{/* Main Content */}
+				<div className='flex flex-1'>
+					{/* Left Section - Form */}
+					<div className='flex flex-1 items-center justify-center px-6 xl:justify-start lg:px-12 xl:px-20'>
+						<div className='w-full max-w-3xl'>
+							<div className='mb-8 sm:mb-10'>
+								<h1 className='text-3xl sm:text-4xl font-bold text-slate-100 mb-2'>
+									Login
+								</h1>
+								<p className='text-sm sm:text-base text-card-foreground'>
+									Entre com seus credenciais para acessar a sua conta.
+								</p>
+							</div>
+							<AuthForm />
+						</div>
+					</div>
+
+					{/* Right Section - Banner Image */}
+					<div className='hidden xl:flex flex-1 items-center justify-center xl:mr-12 2xl:mr-20'>
+						<div className='relative aspect-934/952 w-175'>
+							<div className='absolute top-0 right-[18%] translate-x-1/2 z-10 flex gap-4'>
+								<Button className='p-8 bg-slate-700 rounded-[64px] cursor-pointer'>
+									<Image
+										src='/customer-support.svg'
+										width={22}
+										height={22}
+										alt='Suporte ao cliente'
+									/>
+									Ajuda
+								</Button>
+								<Button className='py-6 px-2 bg-slate-700 rounded-[64px] cursor-pointer'>
+									<Image
+										src='/arrow-down.svg'
+										width={15}
+										height={15}
+										alt='Seta para baixo'
+									/>
+									<Image
+										src='/country-code.svg'
+										width={17}
+										height={17}
+										alt='Bandeira de um país'
+									/>
+									PT br
+								</Button>
+							</div>
+							<Image
+								src='/home-banner.png'
+								alt='Home banner'
+								fill
+								priority
+								className='object-contain'
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
